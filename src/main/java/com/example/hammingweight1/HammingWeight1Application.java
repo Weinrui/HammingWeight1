86212ee4cdbb0d6e3fa0bf4c6a0e0df3ed425c8c
Wenrui LIU <wi22b043@technikum-wien.at>
2023-06-13 12:06:40 +0200

    Initial commit

---
+++ src/main/java/com/example/hammingweight1/HammingWeight1Application.java
@@ -0,0 +1,13 @@
+package com.example.hammingweight1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingWeight1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingWeight1Application.class, args);
+    }
+
+}
